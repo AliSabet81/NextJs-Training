ae288fd22fe9859dccc52f08402f4269580bffc7
AliSabet81 <sabet.farshid1381@gmail.com>
2024-03-30 20:47:27 +0330

    adding menubar component

--- components/shared/Navbar/Theme.tsx
+++ components/shared/Navbar/Theme.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import { useTheme } from "@/context";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 const Theme = () => {
+  const { mode, setMode } = useTheme();
   return (
-    <div>Theme</div>
-  )
-}
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>New Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Print</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+};
 
-export default Theme
+export default Theme;
