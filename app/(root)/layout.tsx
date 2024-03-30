33712706abe8467cc41e181e8f438cd0786a2bb4
AliSabet81 <sabet.farshid1381@gmail.com>
2024-03-30 20:44:06 +0330

    installing shadcn

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/components";
+import { Navbar } from "@/components/shared/Navbar/Navbar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
---
+++ components/shared/Navbar/Theme.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Theme = () => {
+  return (
+    <div>Theme</div>
+  )
+}
+
+export default Theme
