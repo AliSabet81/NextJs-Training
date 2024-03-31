237e99e112b9c82717ed4e076aa4b66302b4ec1e
AliSabet81 <sabet.farshid1381@gmail.com>
2024-03-31 16:07:02 +0330

    right side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,18 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import { Navbar } from "@/components/shared/Navbar/Navbar";
+import RightSideBar from "@/components/shared/RightSideBar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="background-light850_dark100 relative">
-      <Navbar/>
+      <Navbar />
       <div className="flex">
-        <LeftSidebar/>
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSideBar
+        <RightSideBar />
       </div>
       Toaster
     </main>
---
+++ components/shared/RenderTag.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RenderTag = () => {
+  return (
+    <div>RenderTag</div>
+  )
+}
+
+export default RenderTag
