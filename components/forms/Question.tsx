67ea69664e21b88276c4d3271b400999c0d9aa8a
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-01 23:05:08 +0330

    question schema

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,9 +1,26 @@
-'use client'
-
+"use client";
+import { QuestionsSchema } from "@/lib/validations";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField } from "../ui/form";
 const Question = () => {
-  return (
-    <div>Question</div>
-  )
-}
+  const form = useForm<z.infer<typeof QuestionsSchema>>({
+    resolver: zodResolver(QuestionsSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
 
-export default Question
+  const onSubmit = (values: z.infer<typeof QuestionsSchema>) => {};
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField control={form.control} name="title"/>
+      </form>
+    </Form>
+  );
+};
+
+export default Question;
