effe65500c0918f272b1cbecf9552a872f7cecba
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-05 14:42:03 +0330

    update user

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,7 +2,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createUser } from "@/lib/actions/user.action";
+import { createUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -65,7 +65,22 @@ export async function POST(req: Request) {
       picture: image_url,
     });
     return NextResponse.json({ message: "OK", user: mongoUser });
-  } 
+  }
+  if (eventType === "user.updated") {
+    const { id, email_addresses, image_url, username, first_name, last_name } =
+      evt.data;
+    const mongoUser = await updateUser({
+      clerkId: id,
+      updateData: {
+        name: `${first_name}${last_name || ""}`,
+        username: username!,
+        email: email_addresses[0].email_address,
+        picture: image_url,
+      },
+      path: `/profile/${id}`,
+    });
+    return NextResponse.json({ message: "OK", user: mongoUser });
+  }
 
   return new Response("", { status: 200 });
 }
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.mode";
 import { connectToDatabase } from "../mongoose";
-import { CreateUserParams } from "./shared.types";
+import { CreateUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
 
 export const getUserById = async (params: any) => {
   try {
@@ -25,6 +26,19 @@ export const createUser = async (userData: CreateUserParams) => {
 
     const newUser = await User.create(userData);
     return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+export const updateUser = async (userData: UpdateUserParams) => {
+  try {
+    connectToDatabase();
+    const { clerkId, updateData, path } = userData;
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
   } catch (error) {
     console.log(error);
     throw error;
