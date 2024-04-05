6d045c4d4af7f756b79a3878f8bf1e1b7c1ae580
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-05 11:24:48 +0330

    CreateQuestionParams type

--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -1,6 +1,16 @@
+import { IUser } from "@/database/user.mode";
+import { Schema } from "mongoose";
+
 export interface GetQuestionParams {
   page?: number;
   pageSize: number;
   searchQuery?: string;
   filter?: string;
+}
+export interface CreateQuestionParams {
+  title: string;
+  content: string;
+  tags: string[];
+  author: Schema.Types.ObjectId | IUser;
+  path: string;
 }
