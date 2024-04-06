3418ddbe3bc1c4e0991d8d51e4014b7302909029
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-06 23:48:37 +0330

    succesfully connect clerk data base with mongo and create users

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,11 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId = "123456789";
+  const { userId } = auth();
 
   if (!userId) redirect("/sign-in");
 
