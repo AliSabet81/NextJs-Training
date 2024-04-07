6de664f9b8877c186dad87d37ef604cd0b501ecd
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-07 08:38:12 +0330

    starting community

---
+++ app/(root)/community/page.tsx
@@ -0,0 +1,27 @@
+import Filter from "@/components/shared/Filter";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { UserFilters } from "@/constants/filters";
+
+const page = () => {
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/community"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for amazing minds"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={UserFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+      <section className="mt-12 flex flex-wrap gap-4"></section>
+    </>
+  );
+};
+
+export default page;
