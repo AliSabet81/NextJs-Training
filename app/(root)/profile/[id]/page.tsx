286f93073e18a2689f9e36713ca562f925fbd1e2
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-09 16:57:12 +0330

    showing some profile data

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,10 +1,70 @@
+import { Button } from "@/components/ui/button";
 import { getUserInfo } from "@/lib/actions/user.action";
 import { URLProps } from "@/types";
+import { SignedIn, auth } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Page = async ({ params, searchParams }: URLProps) => {
-  const userInfo = await getUserInfo({ userId: params.id });
-  return <div>Page</div>;
+  const { user, totalAnswers, totalQuestions } = await getUserInfo({
+    userId: params.id,
+  });
+  const { userId: clerkId } = auth();
+
+  return (
+    <>
+      <div className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <Image
+            src={user?.picture}
+            alt="profile picture"
+            width={140}
+            height={140}
+            className="rounded-full object-cover"
+          />
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{user.name}</h2>
+            <p className="paragraph-regular text-dark200_light800">
+              @{user.username}
+            </p>
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {user.location && <>location</>}
+              {user.joinedAt?.toString()}
+            </div>
+            {user.bio && <p>{user.bio}</p>}
+          </div>
+        </div>
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          <SignedIn>
+            {clerkId === user.clerkId && (
+              <Link href="/profile/edit">
+                <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-[46px] min-w-[175px] px-4 py-3">
+                  Edit Profile
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+        </div>
+      </div>
+      Stats
+      <div className="mt-10 flex gap-10">
+        <Tabs defaultValue="top-posts" className="flex-1">
+          <TabsList className="background-light800_dark400 min-h-[42px] p-1">
+            <TabsTrigger value="top-posts" className="tab">
+              Top Posts
+            </TabsTrigger>
+            <TabsTrigger value="answers" className="tab">
+              Answers
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="top-posts">Posts</TabsContent>
+          <TabsContent value="answers">Answers</TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 };
 
 export default Page;
