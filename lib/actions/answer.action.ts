71ec2311f4ecf27368d98edab74dd77d001d0355
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-10 13:36:27 +0330

    deleteAnswer action

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -5,10 +5,12 @@ import { connectToDatabase } from "../mongoose";
 import {
   AnswerVoteParams,
   CreateAnswerParams,
+  DeleteAnswerParams,
   GetAnswersParams,
 } from "./shared.types";
 import Question from "@/database/question.model";
 import { revalidatePath } from "next/cache";
+import Interaction from "@/database/interaction.model";
 
 export const createAnswer = async (params: CreateAnswerParams) => {
   try {
@@ -106,6 +108,30 @@ export const downVoteAnswer = async (params: AnswerVoteParams) => {
     if (!answer) {
       throw new Error("Answer not found");
     }
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deleteAnswer = async (params: DeleteAnswerParams) => {
+  try {
+    connectToDatabase();
+
+    const { answerId, path } = params;
+
+    const answer = await Answer.findById(answerId);
+    if (!answer) {
+      throw new Error("Answer not found");
+    }
+    await Answer.deleteOne({ _id: answer });
+    await Question.updateMany(
+      { _id: answer.question },
+      { $pull: { answers: answerId } }
+    );
+    await Interaction.deleteMany({ answer: answerId });
+
     revalidatePath(path);
   } catch (error) {
     console.log(error);
