546528bcf84f0dd04ebd2549cc69886ef66822f4
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-10 17:52:38 +0330

    search question by text in title and content

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,11 +6,12 @@ import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import { getQuestions } from "@/lib/actions/question.action";
+import { SearchParamsProps } from "@/types";
 import Link from "next/link";
 import React from "react";
 
-const Home = async () => {
-  const result = await getQuestions({});
+const Home = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getQuestions({ searchQuery: searchParams.q });
 
   return (
     <>
