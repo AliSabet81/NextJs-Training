c1be66a8710ab4480546551cd55fe7813b7a8a57
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-10 15:55:51 +0330

    Textarea component

--- components/forms/Profile.tsx
+++ components/forms/Profile.tsx
@@ -1,9 +1,141 @@
-import React from 'react'
+"use client";
 
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+interface Params {
+  clerkId: string;
+  user: string;
 }
 
-export default Profile
+const Profile = ({ clerkId, user }: Params) => {
+  const parsedUser = JSON.parse(user);
+  const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="mt-9 flex w-full">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel>
+                Name <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Your name"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel>
+                Username <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Your username"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="portofolioWebsite"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel>Portofolio Link</FormLabel>
+              <FormControl>
+                <Input
+                  type="url"
+                  placeholder="Your portofolio url"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel>Location</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Where are you from?"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel>Bio</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="What's special about you?"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default Profile;
