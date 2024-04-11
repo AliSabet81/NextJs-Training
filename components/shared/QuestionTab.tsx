d31f18dc017a9d4571d485b205755680809a36a8
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-12 01:09:24 +0330

    user profile top post pagination

--- components/shared/QuestionTab.tsx
+++ components/shared/QuestionTab.tsx
@@ -2,6 +2,7 @@ import { getUserQuestions } from "@/lib/actions/user.action";
 import { SearchParamsProps } from "@/types";
 import React from "react";
 import QuestionCard from "../cards/QuestionCard";
+import Pagination from "./Pagination";
 
 interface Props extends SearchParamsProps {
   userId: string;
@@ -9,7 +10,10 @@ interface Props extends SearchParamsProps {
 }
 
 const QuestionTab = async ({ searchParams, userId, clerkId }: Props) => {
-  const result = await getUserQuestions({ userId });
+  const result = await getUserQuestions({
+    userId,
+    page: searchParams.page ? +searchParams.page : 1,
+  });
   return (
     <>
       {result.questions.map((question) => (
@@ -26,6 +30,12 @@ const QuestionTab = async ({ searchParams, userId, clerkId }: Props) => {
           createdAt={question.createdAt}
         />
       ))}
+      <div className="mt-2">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
+      </div>
     </>
   );
 };
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -243,20 +243,23 @@ export const getUserQuestions = async (params: GetUserStatsParams) => {
   try {
     connectToDatabase();
 
-    const { userId } = params;
+    const { userId, page = 1, pageSize = 10 } = params;
+    const skipAmount = (page - 1) * pageSize;
 
     const totalQuestions = await Question.countDocuments({ author: userId });
-    console.log(totalQuestions);
 
     const userQuestions = await Question.find({ author: userId })
       .sort({
         views: -1,
         upvotes: -1,
       })
+      .skip(skipAmount)
+      .limit(pageSize)
       .populate("tags", "_id name")
       .populate("author", "_id clerkId name picture");
 
-    return { questions: userQuestions, totalQuestions };
+    const isNext = totalQuestions > skipAmount + userQuestions.length;
+    return { questions: userQuestions, totalQuestions, isNext };
   } catch (error) {
     console.log(error);
     throw error;
