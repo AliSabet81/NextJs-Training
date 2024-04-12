90bf6a66b5054cfe5db84f0293c4f9a4076aa027
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-12 12:02:28 +0330

    starting global search

--- pagination.ts
+++
@@ -1,6 +0,0 @@
-const { searchQuery, filter, page = 1, pageSize = 20 } = params;
-const skipAmount = (page - 1) * pageSize;
-  .skip(skipAmount)
-      .limit(pageSize)
-const totalQuestions = await Question.countDocuments(query);
-const isNext = totalQuestions > skipAmount + questions.length;
