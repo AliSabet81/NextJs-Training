245118988193d4d44791ac8b41fb712f5edcab3c
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-13 23:55:10 +0330

    community page loading

--- app/(root)/community/loading.tsx
+++ app/(root)/community/loading.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 const Loading = () => {
@@ -5,7 +6,16 @@ const Loading = () => {
     <section>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
       <div className="mb-12 mt-11 flex flex-wrap gap-5">
-        
+        <Skeleton className="h-14 flex-1" />
+        <Skeleton className="h-14 w-28" />
+      </div>
+      <div className="flex flex-wrap gap-4">
+        {Array.from({ length: 10 }).map((item, index) => (
+          <Skeleton
+            key={index}
+            className="h-60 w-full rounded-2xl sm:w-[260px]"
+          />
+        ))}
       </div>
     </section>
   );
