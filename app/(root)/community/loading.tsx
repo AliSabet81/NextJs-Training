5009b7f12e1dfb63d70504d01d6cda1b01d79f7e
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-13 23:51:00 +0330

    adding skeleton component

---
+++ app/(root)/community/loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <section>
+      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+      <div className="mb-12 mt-11 flex flex-wrap gap-5">
+        
+      </div>
+    </section>
+  );
+};
+
+export default Loading;
