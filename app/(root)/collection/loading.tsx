7b1d2eb90e5933f1f0cbb41aead6f30f1c2759ec
AliSabet81 <sabet.farshid1381@gmail.com>
2024-04-14 00:15:18 +0330

    tags page loading

--- app/(root)/collection/loading.tsx
+++ app/(root)/collection/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Loading = () => {
   return (
     <section>
-      <h1 className="h1-bold text-dark100_light900">Saved Question</h1>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
       <div className="mb-12 mt-11 flex flex-wrap gap-5">
         <Skeleton className="h-14 flex-1" />
         <Skeleton className="h-14 w-28" />
--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,5 +1,5 @@
-import { cn } from "@/lib/utils"
-import React from "react"
+import { cn } from "@/lib/utils";
+import React from "react";
 
 function Skeleton({
   className,
@@ -7,10 +7,10 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn("animate-pulse rounded-md bg-light-700", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
